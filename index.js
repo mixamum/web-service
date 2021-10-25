d418ea27cef1794151fb013e7a449b112862ad6d
mixamum <mixamum@users.noreply.github.com>
2021-10-25 15:02:42 -0400

    Test

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
 const mysql = require("mysql");
+const express = require("express");
 
 const json = fs.readFileSync("credentials.json", "utf8");
 const credentials = JSON.parse(json);
 
 const connection = mysql.createConnection(credentials);
+
+const service = express();
+
 connection.connect((error) => {
   if (error) {
     console.error(error);
@@ -12,12 +16,53 @@ connection.connect((error) => {
   }
 });
 
-const selectQuery = "SELECT * FROM memory";
-connection.query(selectQuery, (error, rows) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(rows);
-  }
+const port = 5001;
+service.listen(port, () => {
+  console.log(`We're live on port: ${port}!`);
 });
-connection.end();
+
+function rowToMemory(row) {
+  return {
+    id: row.id,
+    year: row.year,
+    month: row.month,
+    day: row.day,
+    entry: row.entry,
+  };
+}
+
+service.get("/memories/:month/:day", (request, response) => {
+  // issue select query
+  const parameters = [
+    parseInt(request.params.month),
+    parseInt(request.params.day),
+  ];
+
+  const query =
+    "SELECT * FROM memory WHERE month = ? AND day = ? AND is_deleted = 0 ORDER BY year DESC";
+  connection.query(query, parameters, (error, rows) => {
+    if (error) {
+      response.status(500);
+      response.json({
+        ok: false,
+        results: error.message,
+      });
+    } else {
+      const memories = rows.map(rowToMemory);
+      response.json({
+        ok: true,
+        results: rows.map(rowToMemory),
+      });
+    }
+  });
+});
+
+// const selectQuery = "SELECT * FROM memory";
+// connection.query(selectQuery, (error, rows) => {
+//   if (error) {
+//     console.error(error);
+//   } else {
+//     console.log(rows);
+//   }
+// });
+// connection.end();
