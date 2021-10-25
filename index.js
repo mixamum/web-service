3afb696939a2d9802471020ff949530c19e6f3ac
mixamum <mixamum@users.noreply.github.com>
2021-10-25 14:47:48 -0400

    sql connection

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+const fs = require("fs");
+const mysql = require("mysql");
+
+const json = fs.readFileSync("credentials.json", "utf8");
+const credentials = JSON.parse(json);
+
+const connection = mysql.createConnection(credentials);
+connection.connect((error) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+});
+
+const selectQuery = "SELECT * FROM memory";
+connection.query(selectQuery, (error, rows) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(rows);
+  }
+});
+connection.end();
