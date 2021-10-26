a29b7172fde79523ad033793666c9cc734f63b49
mixamum <mixamum@users.noreply.github.com>
2021-10-26 18:33:27 -0400

    fa

--- songsdb.sql
+++ songsdb.sql
@@ -1,12 +1,3 @@
-DROP DATABASE IF EXISTS music;
-DROP USER IF EXISTS music_user@localhost;
-
--- Create Unforget database and user. Ensure Unicode is fully supported.
-CREATE DATABASE music CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-CREATE USER music_user@localhost IDENTIFIED WITH mysql_native_password BY 'M1x&112946';
-GRANT ALL PRIVILEGES ON music.* TO music_user@localhost;
-
-
 DROP TABLE IF EXISTS songs;
 
 CREATE TABLE songs (
