6826b65cb9b7a3276966dd3230d3230139363f8e
mixamum <mixamum@users.noreply.github.com>
2021-10-26 18:48:42 -0400

    get songs/genre

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const fs = require("fs");
 const mysql = require("mysql");
 const express = require("express");
 
-const json = fs.readFileSync("credentials.json", "utf8");
-const credentials = JSON.parse(json);
-
+const credentials = JSON.parse(fs.readFileSync("credentials.json", "utf8"));
 const connection = mysql.createConnection(credentials);
 
 const service = express();
@@ -21,26 +19,21 @@ service.listen(port, () => {
   console.log(`We're live on port: ${port}!`);
 });
 
-function rowToMemory(row) {
+function rowToSongs(row) {
   return {
     id: row.id,
     year: row.year,
-    month: row.month,
-    day: row.day,
-    entry: row.entry,
+    song_name: row.song_name,
+    artist_name: row.artist_name,
+    genre: row.genre,
+    song_length: row.song_length,
   };
 }
 
-service.get("/memories/:month/:day", (request, response) => {
-  // issue select query
-  const parameters = [
-    parseInt(request.params.month),
-    parseInt(request.params.day),
-  ];
-
-  const query =
-    "SELECT * FROM memory WHERE month = ? AND day = ? AND is_deleted = 0 ORDER BY year DESC";
-  connection.query(query, parameters, (error, rows) => {
+service.get("/songs/:genre", (req, res) => {
+  const params = [requests.params.genre];
+  const query = "SELECT * FROM songs WHERE genre = ? AND is_deleted = 0";
+  connection.query(query, params, (error, rows) => {
     if (error) {
       response.status(500);
       response.json({
@@ -48,21 +41,12 @@ service.get("/memories/:month/:day", (request, response) => {
         results: error.message,
       });
     } else {
-      const memories = rows.map(rowToMemory);
+      const memories = rows.map(rowToSongs);
       response.json({
         ok: true,
-        results: rows.map(rowToMemory),
+        results: rows.map(rowToSongs),
       });
     }
   });
 });
 
-// const selectQuery = "SELECT * FROM memory";
-// connection.query(selectQuery, (error, rows) => {
-//   if (error) {
-//     console.error(error);
-//   } else {
-//     console.log(rows);
-//   }
-// });
-// connection.end();
