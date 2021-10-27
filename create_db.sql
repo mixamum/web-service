0d237788bf2437279d932d23280d634317d817aa
mixamum <mixamum@users.noreply.github.com>
2021-10-26 21:44:39 -0400

    das

--- create_db.sql
+++ create_db.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS music;
-DROP USER IF EXISTS music_user@localhost;
+DROP DATABASE IF EXISTS playlist;
+DROP USER IF EXISTS playlist_user@localhost;
 
 -- Create Unforget database and user. Ensure Unicode is fully supported.
-CREATE DATABASE music CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-CREATE USER music_user@localhost IDENTIFIED WITH mysql_native_password BY 'M1x&112946';
-GRANT ALL PRIVILEGES ON music.* TO music_user@localhost;
+CREATE DATABASE playlist CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE USER playlist_user@localhost IDENTIFIED WITH mysql_native_password BY 'M1x&112946';
+GRANT ALL PRIVILEGES ON playlist.* TO playlist_user@localhost;
